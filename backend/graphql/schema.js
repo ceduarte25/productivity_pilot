cba186b921b05acf3efb492002ee4799986063ac
Christian Eduarte <127086054+ceduarte25@users.noreply.github.com>
2024-01-13 18:02:57 +0800

    fixed schema bug and implemented user authentication

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -16,8 +16,15 @@ export const typeDefs = gql`
     password: String
   }
 
+  type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+  }
+
   type Query {
     tasks: [Task!]!
+    login(email: String!, password: String!): AuthData!
   }
 
   type Mutation {
